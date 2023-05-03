acda7c029786b71963513fcfbdbd08eb0969364e
HoracioRomero <130058371+HoracioRomero@users.noreply.github.com>
2023-05-03 10:08:01 +0100

    Added form

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,17 +1,29 @@
 import { Component } from '@angular/core';
-import { CartService } from '../cart.service'
+import { FormBuilder } from '@angular/forms';
 
+import { CartService } from '../cart.service';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+  styleUrls: ['./cart.component.css'],
 })
 export class CartComponent {
-
   items = this.cartService.getItems();
 
-  constructor(private cartService: CartService) {}
-  
-}
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: '',
+  });
 
+  constructor(
+    private formBuilder: FormBuilder,
+    private cartService: CartService
+  ) {}
+
+  onSubmit(): void {
+    this.items = this.cartService.clearCart();
+    console.warn('Your order has been submitted', this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
+}
